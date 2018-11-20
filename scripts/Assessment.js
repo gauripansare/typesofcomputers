05bb5a7af5159cdc7660e77515b11ba8e379066d
atul0030 <atul@knowdl.com>
2018-11-20 17:34:38 +0530

    update file

--- scripts/Assessment.js
+++ scripts/Assessment.js
@@ -272,12 +272,17 @@ var _Assessment = (function () {
 			if (isIE11version) {
 				this.SetCustomarialabelforRadio();
 
-		     }
+			 }
+			 if(gRecordData.Score == undefined || gRecordData.Score == "")
+			 {
+				gRecordData.Score = score;
+			 }
+			 var perscore = gRecordData.Score / parseInt(gRecordData.AssessmentScore) * 100;	
+				$("#ScoreSummary").text("Score: " + perscore + "%");
 			if (gRecordData.Status == "Started") {
 				gRecordData.Status = "Completed";
 				gRecordData.Score = score;
-				var perscore = gRecordData.Score / parseInt(gRecordData.AssessmentScore) * 100;	
-				$("#ScoreSummary").text("Score: " + perscore + "%");
+				
 				this.SetScore(perscore);
 			}
 			if(_Navigator.IsPresenterMode())
